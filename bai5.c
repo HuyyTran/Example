1df69c98ac91e36b2394f62b3d9aae483521bc27
HuyyTran <huytrn.dev@gmail.com>
2021-10-12 20:50:57 +0700

    moi lam phep cong

--- bai5.c
+++ bai5.c
@@ -1,5 +1,19 @@
 #include <stdio.h>
 
+int tong(int a, int b)
+{
+    return a + b;
+}
 void main()
 {
+    int a = 100, b = 1321, c;
+    c = tong(a, b);
 }
+// 1. t sài git ở đây
+// 2. trỏ tới đâu
+// 3. đẩy dữ liệu lên github
+//       - git add . ( chấm có nghĩa là đẩy hết file dưới đây lên)
+//       - git commit -m "" (để lại note)
+//       - git push
+//       - mở folder cần lấy dự án --> chuột phải --> Git Bash here
+//       - git clone + link --> Lấy 1 dự án có sẵn về máy
